edb10b99738b47cecb082bd06cd4ce598383198f
shivanisingh98 <shivanisingh.jbp@gmail.com>
2017-07-21 08:17:24 +0530

    check

--- app/src/main/java/com/example/shivani/pets/data/PetProvider.java
+++ app/src/main/java/com/example/shivani/pets/data/PetProvider.java
@@ -137,7 +137,6 @@ public class PetProvider extends ContentProvider {
         final int match = sUriMatcher.match(uri);
         switch (match) {
             case PETS:
-
                 return updatePet(uri, contentValues, selection, selectionArgs);
             case PET_ID:
                 // For the PET_ID code, extract out the ID from the URI,
